b3a2aabbe2f66e5ca518010a28de5f243efaaa4c
Jana Haberlach <janahaberlach@gmail.com>
2022-02-17 08:51:01 -0500

    correct code

--- headfirstjavascript/playlist.js
+++ headfirstjavascript/playlist.js
@@ -9,13 +9,14 @@ function init() {
 	addValidation();
 }
 
-function handleButtonClick(e) {
+function addValidation(){
 	var loc = window.location.href;
 	var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri='+ loc;
 	var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +loc + '?profile=css3';
 	document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
 	document.getElementById("vLink21").setAttribute("href", CSSvalidLinkStr);
 }
+
 
 function handleButtonClick(e) {
 	var textInput = document.getElementById("songTextInput");
@@ -33,3 +34,4 @@ function handleButtonClick(e) {
 		textInput.value = "";
 	}
 }
+
