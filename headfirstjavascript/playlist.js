7bd8c678644c9e7935fda03948716ba3f3d96ebf
Jana Haberlach <janahaberlach@gmail.com>
2022-02-17 09:25:26 -0500

    changed 21 into 2

--- headfirstjavascript/playlist.js
+++ headfirstjavascript/playlist.js
@@ -14,7 +14,7 @@ function addValidation(){
 	var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
 	var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' + loc + '?profile=css3';
 	document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
-	document.getElementById("vLink21").setAttribute("href", CSSvalidLinkStr);
+	document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
 }
 
 
